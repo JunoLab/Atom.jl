e75eeda49d7594bafc1a7450fbc9737a54d466be
Mike Innes <mike.j.innes@gmail.com>
2015-07-14 22:11:06 +0100

    basic json messaging in place

--- src/AtomClient.jl
+++ src/AtomClient.jl
@@ -1,5 +1,23 @@
 module AtomClient
 
-# package code goes here
+using Lazy, JSON
+
+global sock = nothing
+
+isactive(sock::Nothing) = false
+isactive(sock) = isopen(sock)
+
+function connect(port)
+  global sock = Base.connect(port)
+  @schedule while isopen(sock)
+    (t, data) = JSON.parse(sock)
+    @show t data
+  end
+end
+
+function msg(t, data)
+  isactive(sock) || return
+  println(sock, json(c(t, data)))
+end
 
 end # module
