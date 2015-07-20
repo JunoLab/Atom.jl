db8a5dee65167087f7fcbd97b06036df66b4e55b
Mike Innes <mike.j.innes@gmail.com>
2015-07-20 17:31:26 +0100

    module retreival

--- src/eval.jl
+++ src/eval.jl
@@ -4,6 +4,22 @@ import CodeTools: getblock
 
 LNR.cursor(data::Associative) = cursor(data["row"], data["column"])
 
+handle("module") do data
+  main = haskey(data, "held") ? data["held"] :
+         haskey(data, "path") ? CodeTools.filemodule(data["path"]) :
+         "Main"
+  main == "" && (main = "Main")
+  sub = CodeTools.codemodule(data["code"], cursor(data))
+  return @d(:main => main,
+            :sub  => sub,
+            :inactive => CodeTools.getthing(main) == nothing,
+            :subInactive => CodeTools.getthing("$main.$sub") == nothing)
+end
+
+handle("all-modules") do _
+  [string(m) for m in CodeTools.allchildren(Main)]
+end
+
 handle("eval-block") do data
   code = data["code"]
   block, bounds = getblock(code, data["row"])
