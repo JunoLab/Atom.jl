c00f06a49341bb8cbb73fe6d8049538c180fe0ce
Mike Innes <mike.j.innes@gmail.com>
2015-07-21 13:49:24 +0100

    better eval

--- src/eval.jl
+++ src/eval.jl
@@ -1,27 +1,42 @@
 using CodeTools, LNR
 
-import CodeTools: getblock
+import CodeTools: getblock, getthing
 
 LNR.cursor(data::Associative) = cursor(data["row"], data["column"])
 
-handle("module") do data
-  main = haskey(data, "held") ? data["held"] :
+function modulenames(data, pos)
+  main = haskey(data, "module") ? data["module"] :
          haskey(data, "path") ? CodeTools.filemodule(data["path"]) :
          "Main"
   main == "" && (main = "Main")
-  sub = CodeTools.codemodule(data["code"], cursor(data))
+  sub = CodeTools.codemodule(data["code"], pos)
+  main, sub
+end
+
+function getmodule(data, pos)
+  main, sub = modulenames(data, pos)
+  getthing("$main.$sub", getthing(main, Main))
+end
+
+handle("module") do data
+  main, sub = modulenames(data, cursor(data))
   return @d(:main => main,
             :sub  => sub,
-            :inactive => CodeTools.getthing(main) == nothing,
-            :subInactive => CodeTools.getthing("$main.$sub") == nothing)
+            :inactive => getthing(main) == nothing,
+            :subInactive => getthing("$main.$sub") == nothing)
 end
 
 handle("all-modules") do _
   [string(m) for m in CodeTools.allchildren(Main)]
 end
 
-handle("eval-block") do data
-  code = data["code"]
-  block, bounds = getblock(code, data["row"])
-  display(include_string(Main, block))
+isselection(data) = data["start"] ≠ data["end"]
+
+handle("eval") do data
+  mod = getmodule(data, cursor(data["start"]))
+  block, (start, stop) = isselection(data) ?
+                    getblock(data["code"], cursor(data["start"]), cursor(data["end"])) :
+                    getblock(data["code"], data["start"]["row"])
+  display(include_string(mod, block, get(data, "path", "untitled"), start))
+  start, stop
 end
