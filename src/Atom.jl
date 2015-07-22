a93d147a62e0749e395b9b054fa3a583d97903f0
Mike Innes <mike.j.innes@gmail.com>
2015-07-22 15:46:57 +0100

    handle all errors

--- src/Atom.jl
+++ src/Atom.jl
@@ -16,17 +16,19 @@ handle(f, t) = handlers[t] = f
 function connect(port)
   global sock = Base.connect(port)
   @async while isopen(sock)
-    result, id = nothing, nothing
-    (t, data) = JSON.parse(sock)
-    haskey(data, "callback") && (id = data["callback"])
-    if haskey(handlers, t)
-      try result = handlers[t](data)
-      catch e
-        msg("error", @d(:msg=>"Julia Client – Internal Error",
-                        :detail=>sprint(showerror, e, catch_backtrace())))
+    try
+      result, id = nothing, nothing
+      (t, data) = JSON.parse(sock)
+      haskey(data, "callback") && (id = data["callback"])
+      delete!(data, "callback")
+      if haskey(handlers, t)
+        result = handlers[t](data)
       end
+      isa(id, Integer) && msg(id, result)
+    catch e
+      msg("error", @d(:msg=>"Julia Client – Internal Error",
+                      :detail=>sprint(showerror, e, catch_backtrace())))
     end
-    isa(id, Integer) && msg(id, result)
   end
 end
 
@@ -36,6 +38,7 @@ function msg(t, data)
 end
 
 include("eval.jl")
+include("completions.jl")
 include("misc.jl")
 
 end # module
