79c8c8a6bc465f41ed4ed1f7cc94e6ab4f37b301
Mike Innes <mike.j.innes@gmail.com>
2016-01-24 17:10:13 +0000

    move hook to requires

--- src/patch.jl
+++ src/patch.jl
@@ -1,11 +1,10 @@
 eval(Base, quote
 import Atom
-import MacroTools
 end)
 
 eval(Base, quote
 
-MacroTools.@hook function info(msg...)
+Requires.@hook function info(msg...)
   if Atom.isconnected()
     Atom.@msg info(string(msg...))
   else
