5e546ee0a00c9d3c23bcea1f50ae2cb7188c3289
Mike Innes <mike.j.innes@gmail.com>
2016-02-10 17:55:47 +0000

    new completions approach

--- src/completions.jl
+++ src/completions.jl
@@ -1,13 +1,4 @@
-# TODO: some caching, since this works poorly when there
-# are lots of files around
-
-import CodeTools: allcompletions, text
-
 handle("completions") do data
-  @destruct [pos = cursor(:cursor), code, path || nothing] = data
-  mod = getmodule(data, pos)
-  completions = allcompletions(code, pos, mod = mod, file = path)
-  completions == nothing && return []
-  # completions = @>> completions filter(c->ismatch(r"^\w+$", text(c)))
-  return completions
+  @destruct [mod, line, column] = data
+  CodeTools.completions(line[1:column-1], getthing(mod))
 end
