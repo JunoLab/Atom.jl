ed29d8cbf79cede477f4755c64253759df7c27f7
Mike Innes <mike.j.innes@gmail.com>
2016-02-10 19:58:09 +0000

    send prefix to atom, more filtering

--- src/completions.jl
+++ src/completions.jl
@@ -1,4 +1,13 @@
+matchesprefix(c::AString, pre::AString) = isempty(pre) || lowercase(c[1]) == lowercase(pre[1])
+matchesprefix(c::Dict, pre::AString) = matchesprefix(c[:text], pre)
+matchesprefix(c, ::Void) = true
+
 handle("completions") do data
   @destruct [mod, line, column] = data
-  CodeTools.completions(line[1:column-1], getthing(mod))
+  pre = CodeTools.prefix(line[1:column-1])
+  pre = isempty(pre) ? nothing : pre[end]
+  cs = CodeTools.completions(line[1:column-1], getthing(mod))
+  filter!(c -> matchesprefix(c, pre), cs)
+  d(:completions => cs,
+    :prefix      => pre)
 end
