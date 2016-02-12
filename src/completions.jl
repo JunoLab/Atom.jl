2fb9ba66cce7260df012e4ce9d6d6573630476f8
Mike Innes <mike.j.innes@gmail.com>
2016-02-12 12:43:35 +0000

    make completions a little less enthusiastic

--- src/completions.jl
+++ src/completions.jl
@@ -3,10 +3,11 @@ matchesprefix(c::Dict, pre::AString) = matchesprefix(c[:text], pre)
 matchesprefix(c, ::Void) = true
 
 handle("completions") do data
-  @destruct [mod, line, column] = data
+  @destruct [mod, line, column, force] = data
   pre = CodeTools.prefix(line[1:column-1])
   pre = isempty(pre) ? nothing : pre[end]
-  cs = CodeTools.completions(line[1:column-1], getthing(mod), default = false)
+  cs = (pre == nothing && !force) ? [] :
+    CodeTools.completions(line[1:column-1], getthing(mod), default = false)
   cs ≠ nothing && filter!(c -> matchesprefix(c, pre), cs)
   d(:completions => cs,
     :prefix      => pre,
