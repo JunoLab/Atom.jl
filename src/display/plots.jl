1143a4a04c25e293a48e833fa79001cbd5779821
Mike Innes <mike.j.innes@gmail.com>
2016-02-24 14:41:34 +0000

    images support

--- src/display/plots.jl
+++ src/display/plots.jl
@@ -3,11 +3,21 @@ type PlotPane end
 render(::PlotPane, x; options = d()) =
   @msg plot(render(Inline(), x, options = options))
 
-@require Gadfly begin
-  setdisplay(Gadfly.Plot, PlotPane())
+@init setdisplay(Media.Graphical, PlotPane())
 
+@require Gadfly begin
   @render PlotPane p::Gadfly.Plot begin
     div(d(:style=>"background: white"),
         HTML(stringmime("text/html", p)))
+  end
+end
+
+@require Images begin
+  @render PlotPane img::Images.Image begin
+    HTML() do io
+      print(io, """<img src="data:image/png;base64,""")
+      print(io, stringmime(MIME"image/png"(), img))
+      print(io, "\" />")
+    end
   end
 end
