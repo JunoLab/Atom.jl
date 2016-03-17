3f5e154b5939a71adf621f71327458e4368e26e8
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2016-03-17 19:15:07 +0100

    some preliminary tests

---
+++ test/eval.jl
@@ -0,0 +1,40 @@
+import JSON
+# mock a listener:
+eval(Atom, parse("sock = IOBuffer()"))
+
+# helper
+readmsg() = JSON.parse(ascii(takebuf_array(Atom.sock)))
+
+# callback count
+cb = 0
+
+# check the different message handlers:
+# pingpong
+Atom.handlemsg(Dict("type" => "ping",
+                    "callback" => (cb += 1)))
+@test readmsg() == ["cb", cb, "pong"]
+
+# echo
+Atom.handlemsg(Dict("type" => "echo",
+                    "callback" => (cb += 1)),
+                    "echome!")
+@test readmsg() == ["cb", cb, "echome!"]
+
+# cd
+old_path = pwd()
+Atom.handlemsg(Dict("type" => "cd",
+                    "callback" => (cb += 1)),
+                    joinpath(old_path, ".."))
+@test readmsg() == ["cb", cb, nothing]
+@test pwd() == realpath(joinpath(old_path, ".."))
+cd(old_path)
+
+# evalsimple
+Atom.handlemsg(Dict("type" => "evalsimple",
+                    "callback" => (cb += 1)),
+                    "1+1")
+@test readmsg() == ["cb", cb, 2]
+Atom.handlemsg(Dict("type" => "evalsimple",
+                    "callback" => (cb += 1)),
+                    "sin(pi)")
+@test readmsg() == ["cb", cb, sin(pi)]
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Atom
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# test evaluation capabilities
+include("eval.jl")
