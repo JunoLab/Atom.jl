d4d03f48348acdcf0f480f53bc3e41af312ea2e2
Mike J Innes <mike.j.innes@gmail.com>
2016-10-04 17:38:53 +0100

    no longer need the guard
    since the error will be prevented to the user rather than preventing
    package load

--- src/debugger/load.jl
+++ src/debugger/load.jl
@@ -2,10 +2,7 @@ export Debugger
 
 function __debug__()
   isdefined(Atom, :Debugger) && return
-  # only require the debugger on 0.5.x for now and we aren't on x86 Windows
-  if VERSION.minor == 5 && !(Sys.WORD_SIZE == 32 && is_windows())
-    @eval include(joinpath(dirname($@__FILE__), "debugger.jl"))
-  end
+  @eval include(joinpath(dirname($@__FILE__), "debugger.jl"))
 end
 
 isdebugging() = isdefined(:Debugger) && Debugger.isdebugging()
