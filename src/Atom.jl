23b82643eac3c881139e9908e06117c30920df81
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-02-13 13:55:31 +1300

    reintroduce debugger version check
    for compatibility with 0.6

--- src/Atom.jl
+++ src/Atom.jl
@@ -15,7 +15,11 @@ include("completions.jl")
 include("misc.jl")
 include("frontend.jl")
 include("utils.jl")
-include("debugger/load.jl")
+
+# Gallium.jl only works on 0.5
+if VERSION.minor == 5
+  include("debugger/load.jl")
+end
 include("profiler/profiler.jl")
 
 include("blink/BlinkDisplay.jl")
