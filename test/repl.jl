0cc49358c5155bc4df93f6e504f44dbbe13ef23f
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-01-15 10:58:05 +0100

    fix repl tests

--- test/repl.jl
+++ test/repl.jl
@@ -1,3 +1,10 @@
-@test Atom.didWriteToREPL(() -> print(STDERR, "Atom REPL STDERR test")) == (nothing, true)
-@test Atom.didWriteToREPL(() -> println("Atom REPL STDOUT test")) == (nothing, true)
-@test Atom.didWriteToREPL(() -> 1+1) == (2, false)
+using Atom: didWriteToREPL
+
+@test didWriteToREPL(() -> print(STDERR, "Atom REPL STDERR test")) == (nothing, true, false)
+@test didWriteToREPL(() -> println("Atom REPL STDOUT test")) == (nothing, true, true)
+@test didWriteToREPL(() -> print("Atom REPL STDOUT \n test")) == (nothing, true, false)
+@test didWriteToREPL(() -> begin
+    print("Atom REPL STDOUT test")
+    print(STDERR, "foo\n")
+end) == (nothing, true, true)
+@test didWriteToREPL(() -> 1+1) == (2, false, false)
