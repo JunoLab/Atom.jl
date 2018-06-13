9ecee98ae8e898c7da78d8e0b42591d999d92720
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-06-13 16:42:52 +0200

    option for disabling plotpane from Atom

--- src/misc.jl
+++ src/misc.jl
@@ -22,6 +22,13 @@ handle("exit") do
   exit()
 end
 
+handle("enableplotpane") do enable
+  enable ?
+    Media.setdisplay(Media.Graphical, PlotPane()) :
+    Media.unsetdisplay(Media.Graphical)
+  nothing
+end
+
 handle("cancelCallback") do args...
   # TODO: Do something sensible here.
   # Until then it's better to silently fail than spam the REPL with "unrecognised
