24b8c2ab8a6fc6d6702f280f7e72b38139195ea9
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-07-31 09:49:35 +0200

    richer completions

--- src/completions.jl
+++ src/completions.jl
@@ -22,23 +22,61 @@ function basecompletionadapter(line, mod)
   pre = line[cs[2]]
   d = []
   for c in cs[1]
-    push!(d, Dict{Any, Any}(:type => completiontype(line, cs[2], c, mod),
-                  :description => nothing,
-                  :rightLabel => completionmodule(mod, c),
-                  :text => string(REPLCompletions.completion_text(c)),
-                  :displayText => nothing))
+    # TODO: get local completions from CSTParser or similar
+    # TODO: would be cool to provide `descriptionMoreURL` here to open the docpane
+    push!(d, completion(mod, line, c))
   end
   d, pre
 end
 
+function completion(mod, line, c)
+  return Dict(:type => completiontype(line, c, mod),
+              :description => nothing,
+              :rightLabel => completionmodule(mod, c),
+              :leftLabel => returntype(mod, line, c),
+              :text => completiontext(c),
+              :description => completionsummary(mod, c))
+end
+
+completiontext(x) = REPLCompletions.completion_text(x)
+function completiontext(x::REPLCompletions.MethodCompletion)
+  ct = REPLCompletions.completion_text(x)
+  ct = match(r"^(.*) in .*$", ct)
+  ct isa Nothing ? ct : ct[1]
+end
+
+returntype(mod, line, c) = ""
+function returntype(mod, line, c::REPLCompletions.MethodCompletion)
+  m = c.method
+  atypes = m.sig
+  sparams = m.sparam_syms
+  wa = Core.Compiler.Params(typemax(UInt))  # world age
+  inf = Core.Compiler.typeinf_type(m, atypes, sparams, wa)
+
+  strlimit(string(inf), 20)
+end
+
+strlimit(str::AbstractString, limit = 30) = lastindex(str) > limit ?  str[1:prevind(str, limit)]*"…" : str
+
+using Base.Docs
+function completionsummary(mod, c)
+  ct = REPLCompletions.completion_text(c)
+  b = Docs.Binding(mod, Symbol(ct))
+  CodeTools.description(b)
+end
+
+function completionsummary(mod, c::REPLCompletions.MethodCompletion)
 function completionmodule(mod, c)
   c isa REPLCompletions.ModuleCompletion ? string(c.parent) : string(mod)
 end
 
-function completiontype(line, inds, x, mod)
+function completiontype(line, x, mod)
+  ct = REPLCompletions.completion_text(x)
+  startswith(ct, '@') && return "macro"
+
   if x isa REPLCompletions.ModuleCompletion
     t, f = try
-      str = string(line[1:(first(inds)-1)], REPLCompletions.completion_text(x))
+      str = string(line[1:(first(inds)-1)], ct)
       REPLCompletions.get_type(Meta.parse(str, raise=false, depwarn=false), mod)
     catch e
       nothing, false
@@ -52,7 +90,7 @@ function completiontype(line, inds, x, mod)
     x isa REPLCompletions.PropertyCompletion ? "property" :
     x isa REPLCompletions.FieldCompletion ? "field" :
     x isa REPLCompletions.MethodCompletion ? "λ" :
-    " "
+    "v"
 end
 
 function completiontype(x)
