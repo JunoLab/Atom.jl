f073c8bf5f22f357811ca22186a1dd1cd2d6e6c0
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-03-29 16:01:44 +0100

    handle attempts at recursive debugging gracefully

--- src/debugger/debugger.jl
+++ src/debugger/debugger.jl
@@ -1,5 +1,9 @@
 function enter(mod, ex; initial_continue = false)
-  JunoDebugger.enter(mod, ex; initial_continue = initial_continue)
+  if isdebugging()
+    Base.printstyled(stderr, "Can't debug while debugging.\n", color=Base.error_color())
+  else
+    JunoDebugger.enter(mod, ex; initial_continue = initial_continue)
+  end
 end
 
 isdebugging() = JunoDebugger.isdebugging()
