03313f8fc9a124a71ddc5b42a38fc0db5b280f01
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-04-16 12:09:55 +0200

    make stdlib paths clickable in repl

--- src/path_matching.jl
+++ src/path_matching.jl
@@ -4,6 +4,12 @@ else
   r"((?:(?:\.\.?|\~)|(?:[^\0\s!$`&*()\[\]+'\":;\\])+)?(?:\/(?:[^\0\s!$`&*()\[\]+'\":;\\])+)+)(?:\:(\d+))?"
 end
 
+buildbot_regex = if Sys.iswindows()
+  r"C:\\cygwin\home\\Admininstrator\\buildbot\\.*?(\\julia\\stdlib\\.*?\.jl)"
+else
+  r"\/buildworker\/worker\/.*?(\/julia\/stdlib\/.*?\.jl)"
+end
+
 repl_at_regex = r"@ (?:[^\s]+)\s(.*?)\:(\d+)"
 
 function fullREPLpath(uri)
@@ -14,8 +20,18 @@ function fullREPLpath(uri)
     urimatch = match(uri_regex, uri)
     if urimatch ≠ nothing
       line = urimatch[2] ≠ nothing ? parse(Int, urimatch[2]) : 0
-      return Atom.fullpath(normpath(expanduser(String(urimatch[1])))), line
+      path = fix_buildbot_path(String(urimatch[1]))
+      return Atom.expandpath(path)[2], line
     end
   end
   return "", 0
+end
+
+function fix_buildbot_path(path)
+  urimatch = match(buildbot_regex, path)
+  if urimatch ≠ nothing
+    path = urimatch[1]
+  end
+
+  return path
 end
