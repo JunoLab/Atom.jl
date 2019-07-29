c67f81765d670e1c0f86ee5642f84dec09e8196c
aviatesk <aviatesk@gmail.com>
2019-07-29 17:34:57 +0900

    Make the function definition order more consistent with other parts

--- src/datatip.jl
+++ src/datatip.jl
@@ -1,14 +1,50 @@
 """
-Regex to match code blocks from markdown texts.
-"""
-const codeblock_regex = r"```((?!```).)*?```"s
-"""
 Regex that matches for a Julia documentation text when there is no binding exists
 for the target word.
 Refered to https://github.com/JuliaLang/julia/blob/master/stdlib/REPL/src/docview.jl#L152.
 """
 const nobinding_regex = r"No documentation found.\n\nBinding `.*` does not exist.\n"
 
+handle("datatip") do data
+  @destruct [mod || "Main", word] = data
+  docs = @errs getdocs(mod, word)
+
+  docs isa EvalError && return Dict(:error => true)
+  # @FIXME: This is another horrible hack, may not be rubust to future documentation change.
+  match(nobinding_regex, string(docs)) isa RegexMatch && return Dict(:novariable => true)
+
+  mtable = try getmethods(mod, word)
+    catch e
+      []
+    end
+
+  Dict(:error   => false,
+       :strings => makedatatip(docs, word, mtable))
+end
+
+function makedatatip(docs, word, mtable)
+  # @FIXME?: Separates code blocks from the other markdown texts in order to
+  #          render them as code snippet text by atom-ide-ui's datatip service.
+  #          Setting up functions to deconstruct each `Markdown.MD.content`
+  #          into an appropriate markdown string might be preferred.
+  texts = split(string(docs), codeblock_regex)
+  codes = searchcodeblocks(docs)
+
+  datatips = []
+  processmdtext!(texts[1], datatips)
+  for (code, text) in zip(codes, texts[2:end])
+    processmdcode!(code, datatips)
+    processmdtext!(text, datatips)
+  end
+  processmethodtable!(word, mtable, datatips)
+
+  datatips
+end
+
+"""
+Regex to match code blocks from markdown texts.
+"""
+const codeblock_regex = r"```((?!```).)*?```"s
 # Extract only code blocks from Markdown.MD
 function searchcodeblocks(docs)
   codeblocks = []
@@ -52,40 +88,4 @@ function processmethodtable!(word, mtable, datatips)
 
   push!(datatips, Dict(:type  => :markdown,
                        :value => header * body))
-end
-
-function makedatatip(docs, word, mtable)
-  # @FIXME?: Separates code blocks from the other markdown texts in order to
-  #          render them as code snippet text by atom-ide-ui's datatip service.
-  #          Setting up functions to deconstruct each `Markdown.MD.content`
-  #          into an appropriate markdown string might be preferred.
-  texts = split(string(docs), codeblock_regex)
-  codes = searchcodeblocks(docs)
-
-  datatips = []
-  processmdtext!(texts[1], datatips)
-  for (code, text) in zip(codes, texts[2:end])
-    processmdcode!(code, datatips)
-    processmdtext!(text, datatips)
-  end
-  processmethodtable!(word, mtable, datatips)
-
-  datatips
-end
-
-handle("datatip") do data
-  @destruct [mod || "Main", word] = data
-  docs = @errs getdocs(mod, word)
-
-  docs isa EvalError && return Dict(:error => true)
-  # @FIXME: This is another horrible hack, may not be rubust to future documentation change.
-  match(nobinding_regex, string(docs)) isa RegexMatch && return Dict(:novariable => true)
-
-  mtable = try getmethods(mod, word)
-    catch e
-      []
-    end
-
-  Dict(:error   => false,
-       :strings => makedatatip(docs, word, mtable))
 end
