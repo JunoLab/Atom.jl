d0927378fab8132f8cd27d232d7cdb8338a5dfde
aviatesk <aviatesk@gmail.com>
2019-08-17 04:44:59 +0900

    refactor workspace.jl:
    - show deprecates when `juliaOptions.deprecationWarnings` on
    - modify/add icon/type defs and simplify code
    - introduce constantness check

--- src/workspace.jl
+++ src/workspace.jl
@@ -1,48 +1,46 @@
-ismacro(f::Function) = startswith(string(methods(f).mt.name), "@")
+handle("workspace") do data
+  @destruct [mod, hide || true] = data
+  mod = getmodule′(mod)
+  ns = Symbol.(CodeTools.filtervalid(names(mod; all = true)))
+  filter!(ns) do n
+    (!hide || !Base.isdeprecated(mod, n)) && isdefined(mod, n) && n != Symbol(mod)
+  end
+  contexts = [d(:context => string(mod), :items => map(n -> wsitem(mod, n), ns))]
+  isdebugging() && prepend!(contexts, JunoDebugger.contexts())
+  contexts
+end
 
-wstype(x) = ""
-wstype(::Module) = "module"
-wstype(f::Function) = "function"
-wstype(::Type) = "type"
-wstype(::Expr) = "mixin"
-wstype(::Symbol) = "tag"
-wstype(::AbstractString) = "property"
-wstype(::Number) = "constant"
-wstype(::Exception) = "tag"
-
-wsicon(x) = ""
-wsicon(f::Function) = ismacro(f) ? "icon-mention" : ""
-wsicon(::AbstractArray) = "icon-file-binary"
-wsicon(::AbstractVector) = "icon-list-ordered"
-wsicon(::AbstractString) = "icon-quote"
-wsicon(::Expr) = "icon-code"
-wsicon(::Symbol) = "icon-code"
-wsicon(::Exception) = "icon-bug"
-wsicon(::Number) = "n"
-
-wsnamed(name, val) = false
-wsnamed(name, f::Function) = name == methods(f).mt.name
-wsnamed(name, m::Module) = name == module_name(m)
-wsnamed(name, T::DataType) = name == Symbol(T.name)
-
-function wsitem(name, val)
+wsitem(mod::Module, name) = begin
+  val = getfield(mod, name)
   d(:name  => name,
     :value => render′(Inline(), val),
-    :type  => wstype(val),
-    :icon  => wsicon(val))
+    :type  => wstype(mod, name, val),
+    :icon  => wsicon(mod, name, val))
 end
 
-wsitem(mod::Module, name::Symbol) = wsitem(name, getfield(mod, name))
-
-handle("workspace") do mod
-  mod = getmodule′(mod)
-  ns = filter!(x->!Base.isdeprecated(mod, x), Symbol.(CodeTools.filtervalid(names(mod, all=true))))
-  filter!(n -> isdefined(mod, n), ns)
-  # TODO: only filter out imported modules
-  filter!(n -> !isa(getfield(mod, n), Module), ns)
-  contexts = [d(:context => string(mod), :items => map(n -> wsitem(mod, n), ns))]
-  if isdebugging()
-    prepend!(contexts, JunoDebugger.contexts())
-  end
-  return contexts
+wstype(mod, name, val) = begin
+  val isa Function ? "function" :
+    val isa Type ? "type" :
+    val isa Module ? "module" :
+    val isa Expr ? "mixin" :
+    val isa Symbol ? "tag" :
+    val isa Exception ? "mixin" :
+    isconst(mod, name) ? "constant" : "variable"
 end
+
+wsicon(mod, name, val) = begin
+  val isa Function ? (ismacro(val) ? "icon-mention" : "λ") :
+    val isa Type ? "T" :
+    val isa Module ? "icon-package" :
+    val isa Number ? "n" :
+    val isa AbstractVector ? "icon-list-ordered" :
+    val isa AbstractArray ? "icon-file-binary" :
+    val isa AbstractString ? "icon-quote" :
+    val isa Regex ? "icon-quote" :
+    val isa Expr ? "icon-code" :
+    val isa Symbol ? "icon-code" :
+    val isa Exception ? "icon-bug" :
+    isconst(mod, name) ? "c" : "v"
+end
+
+ismacro(f::Function) = startswith(string(methods(f).mt.name), "@")
