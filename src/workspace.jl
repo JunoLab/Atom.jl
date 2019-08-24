05f7e9f0b98e796e682a8d469315de50bdb63bc5
aviatesk <aviatesk@gmail.com>
2019-08-25 08:36:26 +0900

    handle undefined in workspace:
    - e.g.: `@export undef` in a module within `undef` is not defined

--- src/workspace.jl
+++ src/workspace.jl
@@ -2,7 +2,7 @@ handle("workspace") do mod
   mod = getmodule′(mod)
   ns = Symbol.(CodeTools.filtervalid(names(mod; all = true)))
   filter!(ns) do n
-    !Base.isdeprecated(mod, n) && isdefined(mod, n) && n != Symbol(mod)
+    !Base.isdeprecated(mod, n) && n != Symbol(mod)
   end
   contexts = [d(:context => string(mod), :items => map(n -> wsitem(mod, n), ns))]
   isdebugging() && prepend!(contexts, JunoDebugger.contexts())
@@ -10,16 +10,23 @@ handle("workspace") do mod
 end
 
 wsitem(mod, name) = begin
-  val = getfield(mod, name)
+  val = getfield′′(mod, name)
   wsitem(mod, name, val)
 end
 wsitem(mod, name, val) = begin
   d(:name       => name,
     :value      => render′(Inline(), val),
-    :nativetype => DocSeeker.determinetype(mod, name),
+    :nativetype => nativetype(mod, name, val),
     :type       => wstype(mod, name, val),
     :icon       => wsicon(mod, name, val))
 end
+
+# handle undefineds
+struct Undefined end
+getfield′′(mod, name) = isdefined(mod, name) ? getfield(mod, name) : Undefined()
+@render Inline val::Undefined span(".fade", "<undefined>")
+nativetype(mod, name, val) = DocSeeker.determinetype(mod, name)
+nativetype(mod, name, ::Undefined) = "Undefined"
 
 #=
 @NOTE: `wstype` and `wsicon` are also used for completions / docs
@@ -32,6 +39,7 @@ wstype(mod, name, ::Module) = "module"
 wstype(mod, name, ::Expr) = "mixin"
 wstyep(mod, name, ::Symbol) = "tag"
 wstype(mod, name, ::Exception) = "mixin"
+wstype(mod, name, ::Undefined) = "ignored"
 
 wsicon(mod, name, val) = isconst(mod, name) ? "c" : "v"
 wsicon(mod, name, val::Function) = ismacro(val) ? "icon-mention" : "λ"
@@ -46,5 +54,6 @@ wsicon(mod, name, ::Regex) = "icon-quote"
 wsicon(mod, name, ::Expr) = "icon-code"
 wsicon(mod, name, ::Symbol) = "icon-code"
 wsicon(mod, name, ::Exception) = "icon-bug"
+wsicon(mod, name, ::Undefined) = "icon-circle-slash"
 
 ismacro(f::Function) = startswith(string(methods(f).mt.name), "@")
