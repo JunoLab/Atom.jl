e4d7ee49dc54e40b00f8216378f65220d69fe8ae
aviatesk <aviatesk@gmail.com>
2019-10-23 05:08:24 +0900

    global refactor only from its definition

--- src/refactor.jl
+++ src/refactor.jl
@@ -22,20 +22,27 @@ function renamerefactor(
 )
   mod = getmodule(mod)
 
-  # catch field renaming
+  # check on dot accessor
   modnote = if (obj = first(split(full, '.'))) != old
-    if (parentmod = getfield′(mod, obj)) isa Module && parentmod != mod
-      modulenote(old, parentmod)
+    if (parent = getfield′(mod, obj)) isa Module
+      if parent != mod
+        modulenote(old, parent)
+      else
+        ""
+      end
     else
+      # catch field renaming
       return Dict(:warning => "Rename refactoring on a field isn't available: `$obj.$old`")
     end
   else
     ""
   end
 
-  # local refactor only if `old` is really a local binding
-  bind = CSTParser.bindingof(CSTParser.parse(context))
-  if bind === nothing || old != bind.name
+  expr = CSTParser.parse(context)
+  bind = CSTParser.bindingof(expr)
+
+  # local rename refactor if `old` isn't a toplevel binding
+  if bind === nothing || old ≠ bind.name
     try
       refactored = localrefactor(old, new, path, column, row, startrow, context)
       isempty(refactored) || return Dict(
@@ -47,22 +54,34 @@ function renamerefactor(
     end
   end
 
+  # global rename refactor if the local rename refactor didn't happen
   try
     val = getfield′(mod, full)
+
     # catch keyword renaming
     if val isa Undefined && Symbol(old) in keys(Docs.keywords)
       return Dict(:warning => "Keywords can't be renamed: `$old`")
     end
-    # update modnote
-    if isempty(modnote) && applicable(parentmodule, val) && (parentmod = parentmodule(val)) != mod
-      modnote = modulenote(old, parentmod)
+
+    # catch global refactoring not on definition, e.g.: on a call site
+    if bind === nothing || old ≠ bind.name
+      # TODO: `goto` uri
+      return Dict(:info => contextnote(old, mod, context))
     end
-    kind, desc = globalrefactor(old, new, mod)
-    return Dict(
-      kind => kind === :success ?
-        join(("_Global_ rename refactoring `$old` ⟹ `$new` succeeded.", modnote, desc), "\n\n") :
-        desc
-    )
+
+    kind, desc = globalrefactor(old, new, mod, expr)
+
+    # make description
+    if kind === :success
+      # update modnote
+      if isempty(modnote) && applicable(parentmodule, val) && (parent = parentmodule(val)) ≠ mod
+        modnote = modulenote(old, parent)
+      end
+
+      desc = join(("_Global_ rename refactoring `$old` ⟹ `$new` succeeded.", modnote, desc), "\n\n")
+    end
+
+    return Dict(kind => desc)
   catch err
     @error err
   end
@@ -70,8 +89,17 @@ function renamerefactor(
   return Dict(:error => "Rename refactoring `$old` ⟹ `$new` failed")
 end
 
-modulenote(old, parentmod) =
-  "**NOTE**: `$old` is defined in `$parentmod` -- you may need the same rename refactorings in that module as well."
+modulenote(old, parentmod) = """
+  **NOTE**: `$old` is defined in `$parentmod`
+  -- you may need the same rename refactorings in that module as well.
+  """
+
+contextnote(old, mod, context) = """
+  `$old` isn't found in local bindings in the current context:
+  <details><summary>Context</summary><pre><code>$(strip(context))</code></p></details>
+
+  If you want a global rename refactoring on `$mod.$old`, you need to call from its definition.
+  """
 
 # local refactor
 # --------------
@@ -91,35 +119,42 @@ end
 # global refactor
 # ---------------
 
-function globalrefactor(old, new, mod)
+function globalrefactor(old, new, mod, expr)
   entrypath, line = moduledefinition(mod)
   files = modulefiles(entrypath)
 
   with_logger(JunoProgressLogger()) do
-    refactorfiles(old, new, mod, files)
+    refactorfiles(old, new, mod, files, expr)
   end
 end
 
-function refactorfiles(old, new, mod, files)
-  id = "global_rename_refactor_progress"
-  @info "Start global rename refactoring" progress=0 _id=id
+function refactorfiles(old, new, mod, files, expr)
+  ismacro = CSTParser.defines_macro(expr)
+  oldsym = ismacro ? Symbol("@" * old) : Symbol(old)
+  newsym = ismacro ? Symbol("@" * new) : Symbol(new)
 
-  oldsym = Symbol(old)
-  newsym = Symbol(new)
   total  = length(files)
-
   # TODO: enable line location information (the upstream needs to be enhanced)
   refactoredfiles = Set{String}()
 
+  id = "global_rename_refactor_progress"
+  @info "Start global rename refactoring" progress=0 _id=id
+
   for (i, file) ∈ enumerate(files)
     @info "Refactoring: $file ($i / $total)" progress=i/total _id=id
+
     MacroTools.sourcewalk(file) do ex
-      return if ex === oldsym
+      if ex === oldsym
         push!(refactoredfiles, fullpath(file))
         newsym
+      # handle dot (module) accessor
       elseif @capture(ex, m_.$oldsym) && getfield′(mod, Symbol(m)) isa Module
         push!(refactoredfiles, fullpath(file))
         Expr(:., m, newsym)
+      # macro case
+      elseif ismacro && @capture(ex, macro $(Symbol(old))(args__) body_ end)
+        push!(refactoredfiles, fullpath(file))
+        Expr(:macro, :($(Symbol(new))($(args...))), :($body))
       else
         ex
       end
