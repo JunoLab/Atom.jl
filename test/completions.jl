4e6bd7579601d0201e611b2dedc13ed39b0d7b26
aviatesk <aviatesk@gmail.com>
2019-12-20 21:41:01 +0900

    more test tweaks:
    - allow dynamic completion test in a same julia sesstion
    - remove a completion feature test that should be tested in REPL module

--- test/completions.jl
+++ test/completions.jl
@@ -47,43 +47,29 @@
             end |> length == length(methods(push!))
         end
 
-        ## features
+        ## advanced features
         # - dynamic method addition
         # - shows module where the method defined in right label
         # - shows the infered return type in left label
-        let cs = comps(line)
-            @test filter(cs) do c
+        # HACK: this allows testing this in a same julia runtime
+        let mod = Main.eval(:(module $(gensym("tempmod"))
+                struct Singleton end
+            end))
+            @test filter(comps(line)) do c
                 c[:type] == "method" &&
-                c[:rightLabel] == "Atom" &&
+                c[:rightLabel] == string(mod) &&
                 c[:leftLabel] == "String"
             end |> isempty
-        end
-
-        @eval Atom begin
-            import Base: push!
-            push!(::Undefined) = "i'm such a dynamic push!"
-        end
-
-        let cs = comps(line)
-            @test filter(cs) do c
+            @eval mod begin
+                import Base: push!
+                push!(::Singleton) = ""
+            end
+            @test filter(comps(line)) do c
                 c[:type] == "method" &&
-                c[:rightLabel] == "Atom" &&
+                c[:rightLabel] == string(mod) &&
                 c[:leftLabel] == "String" &&
-                c[:text] == "push!(::Atom.Undefined)"
+                c[:text] == "push!(::$(mod).Singleton)"
             end |> !isempty
-        end
-
-        ## advanced
-        line *= "Undefined(), "
-        let cs = comps(line) # in Main: Undefined is not defined -- should show all push! methods
-            @test length(cs) == length(methods(push!))
-        end
-
-        let c = comps(line, mod = Atom)[1] # in Atom: show push!(::Undefined) **first**
-            @test c[:type] == "method"
-            @test c[:rightLabel] == "Atom"
-            @test c[:leftLabel] == "String"
-            @test c[:text] == "push!(::Atom.Undefined)"
         end
     end
 
