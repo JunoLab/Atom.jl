a7ba19ea222ea79838b312fc5a47d959f692ba5c
aminya <aminyahyaabadi74@gmail.com>
2019-12-31 02:23:38 +0330

    using runtests except modules/goto

--- deps/SnoopCompile/snoopBenchmark.jl
+++ deps/SnoopCompile/snoopBenchmark.jl
@@ -3,5 +3,6 @@ using SnoopCompile
 println("load infer benchmark:")
 @snoopiBenchBot "Atom" using Atom
 
+
 println("Examples/Tests infer benchmark")
 @snoopiBenchBot "Atom"
--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,14 +1,31 @@
 using SnoopCompile
 
 # using runtests:
+# @snoopiBot "Atom"
+# using runtests except modules/goto
+################################################################
+# disabling inlcude modules/goto
+a1 = "include(\"modules.jl\")"
+a2 = "include(\"goto.jl\")"
+
+file = open("test/runtests.jl","r")
+testText = Base.read(file, String)
+close(file)
+testEdited = foldl(replace,
+                     (
+                      a1 => "#"*a1,
+                      a2 => "#"*a2,
+                     ),
+                     init = testText)
+file = open("test/runtests.jl","w")
+Base.write(file, testEdited)
+close(file)
+################################################################
+println("Examples/Tests infer benchmark")
 @snoopiBot "Atom"
+################################################################
 
-# if want to add examples:
-#=
-SnoopCompile.addtestdep()
-@snoopiBot "Atom" begin
-  using Atom
-  # Ues examples
-
-end
-=#
+# enabling back inlcude modules/goto
+file = open("test/runtests.jl","w")
+Base.write(file, testText)
+close(file)
