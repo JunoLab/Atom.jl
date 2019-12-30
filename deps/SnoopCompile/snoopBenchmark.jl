01f187fb5ed8570079dc3b3a77be1a920d7523f5
aminya <aminyahyaabadi74@gmail.com>
2019-12-31 01:04:41 +0330

    using snoopBenchBot
    using v2-beta PR bot

--- deps/SnoopCompile/snoopBenchmark.jl
+++ deps/SnoopCompile/snoopBenchmark.jl
@@ -1,30 +1,7 @@
-# dev your package
-
-# comment the precompile part of your package ("include() and _precompile_()")
-# run this benchmark
-# restart Julia
-
-# uncomment the precompile part of your package ("include() and _precompile_()")
-# run this benchmark
-# restart Julia
-
-# now compare the result
-################################################################
 using SnoopCompile
 
-println("Package load time:")
-loadSnoop = @snoopi using Atom
+println("load infer benchmark:")
+@snoopiBenchBot "Atom" using Atom
 
-timesum(loadSnoop)
-
-################################################################
-println("Running Examples/Tests:")
-runSnoop = @snoopi begin
-
-using Atom
-
-include(joinpath(dirname(dirname(pathof(Atom))), "test", "runtests.jl"))
-
-end
-
-timesum(runSnoop)
+println("Examples/Tests infer benchmark")
+@snoopiBenchBot "Atom"
