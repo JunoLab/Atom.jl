a3cd1e446b0ce2808bedc1a9b63f337cc8ff3d17
aminya <aminyahyaabadi74@gmail.com>
2020-01-26 18:07:28 -0600

    precompile modules and goto too

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,26 +1,26 @@
 using SnoopCompile
 
-# using runtests except modules/goto
-################################################################
-# disabling inlcude modules/goto
-a1 = "include(\"modules.jl\")"
-a2 = "include(\"goto.jl\")"
-
-testText = Base.read("test/runtests.jl", String)
-testEdited = foldl(replace,
-                     (
-                      a1 => "#"*a1,
-                      a2 => "#"*a2,
-                     ),
-                     init = testText)
-Base.write("test/runtests.jl", testEdited)
-################################################################
 @snoopiBot BotConfig("Atom", blacklist = ["realpath′"])
+
+
+# template for exlcuding tests files from precompilation (kept for the record)
+# using runtests except badfile
 ################################################################
-# enabling back inlcude modules/goto
-Base.write("test/runtests.jl", testText)
-
-
+# disabling inlcude badfile
+# a1 = "include(\"badfile.jl\")"
+#
+# testText = Base.read("test/runtests.jl", String)
+# testEdited = foldl(replace,
+#                      (
+#                       a1 => "#"*a1,
+#                      ),
+#                      init = testText)
+# Base.write("test/runtests.jl", testEdited)
+################################################################
+# @snoopiBot BotConfig("Atom", blacklist = ["realpath′"])
+################################################################
+# enabling back inlcude goto
+# Base.write("test/runtests.jl", testText)
 ################################################################
 # using runtests:
 # @snoopiBot "Atom"
