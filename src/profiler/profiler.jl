fc26814da11e908b2cfc04c17bcd876d2756433f
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2020-01-27 14:02:55 +0100

    prune profile graph

--- src/profiler/profiler.jl
+++ src/profiler/profiler.jl
@@ -4,19 +4,19 @@ using Profile, JSON, FlameGraphs
 
 import ..Atom:  expandpath, @msg, handle
 
-function tojson(node::FlameGraphs.Node)
+function tojson(node::FlameGraphs.Node, root = false)
   name, path = expandpath(string(node.data.sf.file))
   classes = []
 
   node.data.status & 0x01 ≠ 0 && push!(classes, "isdispatch")
   node.data.status & 0x02 ≠ 0 && push!(classes, "isgc")
-  
+
   Dict(
     :path => path,
     :location => name,
     :func => node.data.sf.func,
     :line => node.data.sf.line,
-    :count => length(node.data.span),
+    :count => root ? sum(length(c.data.span) for c in node) : length(node.data.span),
     :classes => classes,
     :children => [tojson(c) for c in node]
   )
@@ -25,7 +25,64 @@ end
 function profiler(data = Profile.fetch() ;kwargs...)
   graph = FlameGraphs.flamegraph(data ;kwargs...)
   graph === nothing && return
-  @msg profile(tojson(graph))
+  pruneinternal!(graph)
+  prunetask!(graph)
+  @msg profile(tojson(graph, true))
+end
+
+function prunetask!(node)
+  for c in node
+    if (
+          (
+            c.data.sf.func == :task_done_hook &&
+            endswith(string(c.data.sf.file), "task.jl")
+          ) ||
+          (
+            # REPL
+            c.data.sf.func == :eval &&
+            FlameGraphs.isleaf(c) &&
+            endswith(string(c.data.sf.file), "boot.jl")
+          )
+       )
+      FlameGraphs.prunebranch!(c)
+    end
+  end
+end
+
+function pruneinternal!(node)
+  for c in node
+    if (
+        (
+          # REPL evaluation
+          c.data.sf.func == :eval &&
+          c.parent.data.sf.func == :repleval &&
+          endswith(string(c.parent.data.sf.file), "repl.jl")
+        ) ||
+        (
+          # inline evaluation
+          c.data.sf.func == :include_string &&
+          c.parent.data.sf.func == :include_string &&
+          endswith(string(c.parent.parent.data.sf.file), "eval.jl")
+        )
+       )
+
+      # Add children directly to the root node
+      root = node
+      replchild = c
+      while !FlameGraphs.isroot(root)
+          root = root.parent
+          replchild = replchild.parent
+      end
+      FlameGraphs.graftchildren!(root, c)
+      # Eliminate all nodes in between. This might include some that don't
+      # call REPL code, but as this is also internal it seems OK.
+      FlameGraphs.prunebranch!(replchild)
+      return true
+    else
+      pruneinternal!(c) && return true
+    end
+  end
+  return false
 end
 
 handle("loadProfileTrace") do path
