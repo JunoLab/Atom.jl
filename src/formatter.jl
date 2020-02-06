6d8d35afeb1ebb9bb7ff0083d1b7fac18af9f6aa
aviatesk <aviatesk@gmail.com>
2020-02-06 19:46:44 +0900

    accept formatting options

--- src/formatter.jl
+++ src/formatter.jl
@@ -1,8 +1,23 @@
 using JuliaFormatter: format_text
 
 handle("format") do data
-  @destruct [text, indent || 4, margin || 92] = data
+  @destruct [
+    text,
+    indent || 4,
+    margin || 92,
+    always_for_in || false,
+    whitespace_typedefs || false,
+    whitespace_ops_in_indices || false,
+    remove_extra_newlines || false
+  ] = data
 
-  formattedtext = format_text(text, indent = indent, margin = margin)
-  Dict(:formattedtext => formattedtext)
+  return Dict(:formattedtext => format_text(
+    text;
+    indent = indent,
+    margin = margin,
+    always_for_in = always_for_in,
+    whitespace_typedefs = whitespace_typedefs,
+    whitespace_ops_in_indices = whitespace_ops_in_indices,
+    remove_extra_newlines = remove_extra_newlines
+  ))
 end
