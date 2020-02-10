f64e5ed4b2fd15cbfc93ea1bf9c1ff73dc7b3375
Shuhei Kadowaki <aviatesk@gmail.com>
2020-02-11 04:00:50 +0900

    rm automated SnoopCompile

--- deps/SnoopCompile/benchmark.jl
+++
@@ -1,7 +0,0 @@
-using SnoopCompile
-
-@info "Benchmark inference time during package loading"
-@snoopi_bench "Atom" using Atom
-
-@info "Benchmark inference time during running package testsuite"
-@snoopi_bench "Atom"
