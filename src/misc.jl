cc0b3df820c861ae1e038ca74eb6f74fe0fead9b
Shuhei Kadowaki <aviatesk@gmail.com>
2020-05-18 21:05:59 +0900

    improve `evalsimple` handler:
    - use `JunoProgressLogger`
    - use `include_string`

--- src/misc.jl
+++ src/misc.jl
@@ -46,9 +46,12 @@ handle("activateDefaultProject") do
   update_project()
 end
 
-handle("evalsimple") do code
-  Core.eval(Main, Meta.parse(code))
+function evalsimple(code)
+  return with_logger(JunoProgressLogger()) do
+    include_string(Main, code)
+  end
 end
+handle(evalsimple, "evalsimple")
 
 handle("exit") do
   exit()
